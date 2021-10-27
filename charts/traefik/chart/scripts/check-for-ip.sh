f9614158388d618cf51156c37d0e6009c822d828
Jesper Axelsen <jesperbaxelsen@gmail.com>
2021-10-27 11:43:47 +0200

    Removed ALL tests

--- charts/traefik/chart/scripts/check-for-ip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while :; do
-  sleep 10
-  ip=$(kubectl get services ingress-traefik -n ingress --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
-  if [ ! -z "$ip" ]
-  then
-    break
-  fi
-done
