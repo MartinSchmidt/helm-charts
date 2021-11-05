bafaa2718318dc8eeabf490e33146f756ad6a879
Jesper Axelsen <jesperbaxelsen@gmail.com>
2021-11-05 09:25:48 +0100

    Added test container and workflow

--- ci/helm-dependency-version-checker/entrypoint.sh
+++ ci/helm-dependency-version-checker/entrypoint.sh
@@ -1,4 +1,34 @@
 #!/bin/sh -l
-PWD=$(pwd)
-echo "::set-output name=pwd::$PWD"
+
+mkdir /repo && cd /repo
+git clone https://github.com/distributed-technologies/helm-charts.git
+cd helm-charts/charts
+
+for chart in */chart ; do
+    #grep "dependencies:" $chart/Chart.yaml -A 25 
+    
+    DEPENDENCIES=$(helm dependency list $chart/)
+
+    echo "$DEPENDENCIES" | while IFS= read -r line ; do 
+    
+    STRING=$line
+    
+    if [[ "$STRING" != *"STATUS"* ]] && [[ "$STRING" != *"WARNING:"* ]]; then
+        NAME=$(echo $STRING | cut -d " " -f 1)
+        VERSION=$(echo $STRING | cut -d " " -f 2)
+        URL=$(echo $STRING | cut -d " " -f 3)
+        if [ "${URL: -1}" = "/" ]; then
+            INDEXURL="${URL}index.yaml"
+        else
+            INDEXURL="$URL/index.yaml"
+        fi
+        echo $NAME
+        echo $VERSION
+        echo $INDEXURL
+    fi
+    done
+done
+
+
+
 echo "::set-output name=time::$time"
