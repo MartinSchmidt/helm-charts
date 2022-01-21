431b0f928734276c371f9113bea32ef0effad613
Jacob Crawford <jacobcrawford1@hotmail.com>
2022-01-21 11:16:15 +0000

    Fix minor error in kafka test.

--- charts/kafka/chart/scripts/test_ksql.sh
+++ charts/kafka/chart/scripts/test_ksql.sh
@@ -5,4 +5,4 @@ set -e
 pip install ksql
 
 ./scripts/wait_for_pod.sh cp-ksql-server
-python /scripts/python_ksql.py http://$1-cp-ksql-server:8088 
+python /scripts/python_ksql.py http://"$1"-cp-ksql-server:8088 
